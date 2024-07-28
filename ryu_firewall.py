b5ee8bb4050bc4693eabf4e1f2f75731a362f68f
MrSaadKhan <Shadaan127@gmail.com>
2024-07-28 12:01:50 +1000

    Added Controller and topology generation

--- ryu_firewall.py
+++ ryu_firewall.py
@@ -1,1 +1,249 @@
 #construct firewall in this file
+
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib import dpid as dpid_lib
+import ipaddress
+
+class SimpleSwitch13(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+        self.datapaths = {}
+        self.k = 4
+
+
+    def check_ip_in_subnet(ip, subnet):
+        n = ipaddress.ip_network(subnet)
+        netw = int(n.network_address)
+        mask = int(n.netmask)
+        a = int(ipaddress.ip_address(ip))
+        in_network = (a & mask) == netw
+        return in_network
+
+    def get_first_digit(number):
+        number_str = str(abs(number)).replace('.', '')
+        return int(number_str[0])
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Store datapath with a unique identifier
+        switch_name = f'{datapath.id}'  # Adjust this to your naming convention
+        self.datapaths[switch_name] = datapath
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+        # flow_table(self)
+
+        # Make Changes HERE!
+
+        # Route Aggregate and Edge Switches
+        for i in range(self.k):
+            for j in range(self.k):
+                #switch_name = f'agSw{i}{j}'
+                switch_name = f'{datapath.id}'
+                datapath = self.get_datapath(switch_name)
+                dpid_str = dpid_lib.dpid_to_str(datapath.id)
+                if dpid_str[11]==str(i) and dpid_str[13]==str(j):
+                    if datapath and j >= self.k//2:
+                        self.setup_aggregate_switch_flows(datapath, int(str(i) + str(j)))
+                    elif datapath and j < self.k//2:
+                        self.setup_edge_switch_flows(datapath, int(str(i) + str(j)))
+                    else:
+                        print(f"Error: Datapath {datapath} not found for {switch_name}")
+                    
+
+
+        # Route core switches
+
+        #switch_name = f'c_sw{i}'
+        switch_name = f'{datapath.id}'
+        datapath = self.get_datapath(switch_name)
+        dpid_str = dpid_lib.dpid_to_str(datapath.id)
+        if dpid_str[11]==str(self.k): #checks 12th digit to see if this switch is a core switch
+            self.setup_core_switch_flows(datapath)
+
+
+    def setup_core_switch_flows(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid_str = dpid_lib.dpid_to_str(datapath.id)
+        # Example: Installing a flow to forward packets based on switch index
+        for i in range( (self.k // 2) ** 2 ):
+            # Example match criteria and actions for switch 0
+            match = parser.OFPMatch(eth_type=0x0800, ipv4_dst=(f"10.{i}.0.0", '255.255.0.0'))
+            actions = [parser.OFPActionOutput(i+1)]
+
+            # Install the flow entry
+            self.add_flow(datapath, 1, match, actions)
+            # self.logger.info("Added flow to core switch %s match subnet 10.%d.x.x to port %d", dpid_str, i, i+1)
+
+    def setup_aggregate_switch_flows(self, datapath, switch_index):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid_str = dpid_lib.dpid_to_str(datapath.id)
+
+        i = switch_index % 10               # 2nd digit
+        switch_index = switch_index // 10   # 1st digit
+
+        # Agg to Core route - if in from port 3&4, out from ports 1&2
+        for in_port in range(1+self.k//2, 1+self.k):
+            for out_port in range(1, 1+self.k//2):
+                match = parser.OFPMatch(in_port=(in_port))
+                actions = [parser.OFPActionOutput(out_port)]
+                self.add_flow(datapath, 1, match, actions)
+        for j in range(0, self.k//2):
+            ip_DEST = (f"0.0.0.{j+2}", '0.0.0.255')
+            
+            match = parser.OFPMatch(eth_type = 0x0800, ipv4_dst = ip_DEST)
+        
+            #SUFFIX_OUTPUT_PORT_NUM = (j - 2 + y) % (self.k//2) + (self.k//2)
+            actions = [parser.OFPActionOutput(j+1)]
+            
+            self.add_flow(datapath, 1, match, actions)
+
+
+        # Agg to Edge route
+        for j in range(0, self.k//2):
+            ip_DEST = (f"10.{switch_index}.{j}.0", '255.255.255.0')
+            port_DEST = (self.k//2) + j+1
+
+            match = parser.OFPMatch(eth_type = 0x0800, ipv4_dst = ip_DEST)
+            actions = [parser.OFPActionOutput(port_DEST)]
+            self.add_flow(datapath, 10, match, actions)
+            # self.logger.info("Added flow to switch %s to port %d for address %s", dpid_str, port_DEST, ip_DEST[0])
+
+    def setup_edge_switch_flows(self, datapath, switch_index):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        dpid_str = dpid_lib.dpid_to_str(datapath.id)
+
+        i = switch_index % 10               # 2nd digit
+        switch_index = switch_index // 10   # 1st digit
+
+        # Edge to Agg route - if in from port 3&4, out from ports 1&2
+        for in_port in range(1+self.k//2, 1+self.k):
+            for out_port in range(1, 1+self.k//2):
+                match = parser.OFPMatch(in_port=(in_port))
+                actions = [parser.OFPActionOutput(out_port)]
+                self.add_flow(datapath, 1, match, actions)
+
+        for j in range(0, self.k//2):
+            ip_DEST = (f"0.0.0.{j+2}", '0.0.0.255')
+            
+            match = parser.OFPMatch(eth_type = 0x0800, ipv4_dst = ip_DEST)
+        
+            #SUFFIX_OUTPUT_PORT_NUM = (j - 2 + y) % (self.k//2) + (self.k//2)
+            actions = [parser.OFPActionOutput(j+1)]
+            
+            self.add_flow(datapath, 1, match, actions)
+
+        # Edge to Host route
+
+        for j in range(0,  self.k//2): # Number of hosts under a single edge switch
+            #10.pod.switch.id
+            ip_DEST = (f"10.{switch_index}.{i}.{j+2}")
+            port_DEST = (j+3)
+
+            match = parser.OFPMatch(eth_type = 0x0800, ipv4_dst = ip_DEST)
+            actions = [parser.OFPActionOutput(port_DEST)]
+            self.add_flow(datapath, 11, match, actions)
+            self.add_flow(datapath, 10, match, actions)
+            self.logger.info("Added flow to switch %s to port %d for address %s", dpid_str, port_DEST, ip_DEST)
+        
+
+
+    def get_datapath(self, switch_name):
+        return self.datapaths.get(switch_name)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+   
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+
+        dpid = format(datapath.id, "d").zfill(16)
+        self.mac_to_port.setdefault(dpid, {})
+
+        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
--- topo_simple.py
+++ topo_simple.py
@@ -1,29 +1,128 @@
 #topology generation in this file
 
+import sys
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.link import TCLink
+from mininet.cli import CLI
+import argparse
 
 
-switches = []
+core_switches = []
+aggr_switches = []
+edge_switches = []
 hosts = []
 
-class simpleTopo(Topo)
-def __init__(self, k):
-        super(simpleTopo, self).__init__()
+class FatTreeTopo(Topo):
+    def __init__(self, k):
+        super(FatTreeTopo, self).__init__()
         self.k = k
-        self.create_simple()
-def create_fat_tree(self):
+        self.create_fat_tree()
+
+    def create_fat_tree(self):
         k = self.k
         
-        
-        dpid = f"000000000001"
-        switches.append(self.addSwitch(f'sw1', dpid=dpid))
+
+        # Create core switches
+        num_core_switches = (k // 2) ** 2 #(k/2)^2 core switches
+        for i in range(num_core_switches):
+            dpid = f"0000000000{k:02d}{i // (k//2):02d}{i % (k//2):02d}"
+            core_switches.append(self.addSwitch(f'c_sw{i}', dpid=dpid))
+
+        # Create pods
+        for pod in range(k): #there are k pods
+            aggr_switches.append([])
+            edge_switches.append([])
+            for sw in range(k // 2): #2 layers of k/2 switches
+                aggr_dpid = f"0000000000{pod:02d}{k//2 + sw:02d}01"
+                edge_dpid = f"0000000000{pod:02d}{sw:02d}01"
+                aggr_switches[pod].append(self.addSwitch(f'agSw{pod}{sw}', dpid=aggr_dpid))
+                edge_switches[pod].append(self.addSwitch(f'edSw{pod}{sw}', dpid=edge_dpid))
+
+            # Connect core and aggregation switches
+            for i in range(k // 2):
+                for j in range(k // 2):
+                    core_index = (i * (k // 2) + j)
+                    self.addLink(core_switches[core_index], aggr_switches[pod][i])
+
+            # Connect aggregation and edge switches within each pod
+            for aggr_sw in aggr_switches[pod]:
+                for edge_sw in edge_switches[pod]:
+                    self.addLink(aggr_sw, edge_sw)
+
+            # Create and connect hosts
+            sw_index = 0
+            offset = 0
+            for host_id in range((k**3//4)//k):
+                # print(f"host:{host_id+1} connecting to switch: {sw_index}, condition:{ (host_id+1) % (k//2) == 0}")
+                host_ip = f"10.{pod}.{sw}.{host_id+2}"
+                host_ip = f"10.{pod}.{sw_index}.{host_id+2-offset}"
+                host = self.addHost(f'h{pod}{host_id}', ip=host_ip)
+                self.addLink(host, edge_switches[pod][sw_index])
+                hosts.append(host)
+                if (host_id+1) % (k//2) == 0:
+                    sw_index += 1
+                    offset += self.k ** 2//8
 
 
-        # Create and connect hosts
+import subprocess
+
+def generate_and_visualize_topology(topo, filename='topology', output_format='png'):
+    """
+    Generates a DOT file and visualizes it using Graphviz.
+
+    Parameters:
+    - topo: Mininet Topo object representing the network topology.
+    - filename: Base filename for output image file (default: 'topology').
+    - output_format: Output format for visualization ('png', 'pdf', etc.) (default: 'png').
+    """
+    dot_filename = f"{filename}.dot"
+
+    # Generate DOT file
+    with open(dot_filename, 'w') as f:
+        f.write('graph {\n')
+        f.write('    rankdir=TB;\n')  # Set layout direction to Top to Bottom
         
-        for host_id in range(k):
-            host_ip = f"10.0.0.{host_id}"
-            host = self.addHost(f'h{host_id}', ip=host_ip)
-            self.addLink(host, switches[0])
-            hosts.append(host)
-      
+        # Add switches and links
+        for switch in topo.switches():
+            f.write(f'    {switch};\n')
+        
+        for link in topo.links(withInfo=True):
+            f.write(f'    {link[0]} -- {link[1]};\n')
+
+        # Add hosts at the bottom
+        f.write('    subgraph hosts {\n')
+        f.write('        rank=same;\n')
+        for host in topo.hosts():
+            f.write(f'        {host};\n')
+        f.write('    }\n')
+
+        f.write('}\n')
+
+    # Visualize using Graphviz
+    output_file = f"{filename}.{output_format}"
+    subprocess.run(['dot', f'-T{output_format}', dot_filename, f'-o{output_file}'])
+
+
+def run_fat_tree(k):
+    show_graph = 0
+    topo = FatTreeTopo(k)
+    net = Mininet(topo, link=TCLink, controller=None, autoSetMacs=True, autoStaticArp=True)
+    net.addController('controller', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")
+    net.start()
+    if show_graph: 
+        generate_and_visualize_topology(topo)
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a Fat-Tree topology with k-parameter')
+    parser.add_argument('k', type=int, help='Fat-Tree parameter k (must be an even number)')
+    args = parser.parse_args()
+
+    if args.k % 2 != 0:
+        print("Error: k must be an even number.")
+        sys.exit(1)
+
+    run_fat_tree(args.k)
