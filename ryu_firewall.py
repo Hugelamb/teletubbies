89df47572c10f1729ca42573fd28669d74bffedc
Yiming <kryptonuclei@gmail.com>
2024-08-02 00:01:08 +1000

    merged visual and firewall

--- ryu_firewall.py
+++ ryu_firewall.py
@@ -10,6 +10,7 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 from ryu.lib import dpid as dpid_lib
 from ryu.lib import hub
+import visualize
 import ipaddress
 
 class SimpleSwitch13(app_manager.RyuApp):
@@ -24,10 +25,10 @@ class SimpleSwitch13(app_manager.RyuApp):
         # firewall variables
         self.monitor_thread = hub.spawn(self.monitor)
         self.src_mac = [] # list of source mac addresses to track
-        self.dst_mac = [] # list of destination mac addresses to track
+        self.dst_ip = {} # dict of destination IPv4 addresses to track and it's count
         self.count_src = [] # number of times each src has sent requests
         self.count_dst = [] # number of times each dst has received packets
-        self.link_max = 5 # set the max. number of packets a link can receive within a window
+        self.link_max = 50 # set the max. number of packets a link can receive within a window
         self.byte_ratio_max = 100 # set min. number of bytes per packet
         self.dst_max = 2 # set the max. number of times a dst can receive packets within a window
 
@@ -64,8 +65,12 @@ class SimpleSwitch13(app_manager.RyuApp):
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
-        self.add_flow(datapath, 0, match, actions)
+        self.add_flow(datapath, 0, match, actions, idle_timeout=0)
         # flow_table(self)
+
+        if datapath.id not in self.datapaths:
+            self.datapaths[datapath.id] = datapath
+            # self.logger.info('Registering datapath: %16x', datapath.id)
 
         # Make Changes HERE!
 
@@ -171,25 +176,28 @@ class SimpleSwitch13(app_manager.RyuApp):
             actions = [parser.OFPActionOutput(port_DEST)]
             self.add_flow(datapath, 10, match, actions)
             self.logger.info("Added flow to switch %s to port %d for address %s", dpid_str, port_DEST, ip_DEST)
-           
+            
 
 
     def get_datapath(self, switch_name):
         return self.datapaths.get(switch_name)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None, idle_timeout=0, reset_count=False):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
+        flags = 0
+        if reset_count:
+            flags |= ofproto.OFPFF_RESET_COUNTS
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
-                                    instructions=inst)
+                                    instructions=inst, idle_timeout=idle_timeout, flags=flags)
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+                                    match=match, instructions=inst, idle_timeout=idle_timeout, flags=flags)
         datapath.send_msg(mod)
    
 
@@ -217,8 +225,8 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
-
-        # self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        if int(dst[:1]) != 3:
+            self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -229,7 +237,7 @@ class SimpleSwitch13(app_manager.RyuApp):
             out_port = ofproto.OFPP_FLOOD
 
         # increment 
-        self.add_dst(src, dst)
+        # self.add_dst(src, dst)
         actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
@@ -238,10 +246,10 @@ class SimpleSwitch13(app_manager.RyuApp):
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
-                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id, 0)
                 return
             else:
-                self.add_flow(datapath, 1, match, actions)
+                self.add_flow(datapath, 1, match, actions, 0)
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
@@ -258,7 +266,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         while True:
             for dp in self.datapaths.values():
                 self.get_stats(dp)
-            hub.sleep(1) # check the throughput every 1 second
+            hub.sleep(20) # check the throughput every 1 second
 
     '''
     get_stats()
@@ -267,7 +275,7 @@ class SimpleSwitch13(app_manager.RyuApp):
     def get_stats(self, datapath):
         self.logger.info('Requesting stats for: %016x', datapath.id)
 
-        ofproto = datapath.ofproto
+        # ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         request = parser.OFPFlowStatsRequest(datapath)
@@ -296,62 +304,75 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        print('Flow stats received...')
+
         # sort flow rules, then check the stats of each rule
-        for stat in msg.body:
-            print(f"{stat}")
-            # filter out by flow rules that have matched
+        for stat in sorted([flow for flow in msg.body if (flow.priority == 10)  and 'ipv4_dst' in flow.match], key=lambda flow: flow.match['ipv4_dst']):
+            print(stat.match)
+            # filter out by flow rules that have matchedoffending handler [handle_flow_stats] servicing event [EventOFPFlowStatsReply] follows.
+            print('priority passed...')
+            print(stat)
+            if stat.packet_count == 0:
+                ip_dst = stat.match['ipv4_dst']
+                if ip_dst not in self.dst_ip:
+                    # self.dst_ip.append(ip_dst)
+                    # self.count_dst.append(1)
+                    self.dst_ip[ip_dst] = 0
+                    print('added ip for: ', ip_dst)
+                # else:
+                #     self.dst_ip[ip_dst] += 0
+
             if stat.packet_count > 0:
-                if len(self.dst_mac) > 0:
-                    # check each dst mac in list and update respective counts
-                    print(f"{self.dst_mac}")
-                    for i in range(len(self.dst_mac)):
-                        if 'eth_dst' in stat.match:
-                            print(f"{stat.match}")
-                            print('eth destination is ', stat.match['eth_dst'])
-                            if stat.match['eth_dst'] == self.dst_mac[i]: self.count_dst[i] += 1
-                
-                # check against firewall conditions
-                packet_condition = stat.packet_count >= self.link_max
-                byte_condition = (stat.byte_count / stat.packet_count) < self.byte_ratio_max
+                print('packet count passed...')
+                if len(self.dst_ip) > 0:
+                    # check each dst ip in list and update respective counts
+                    if 'ipv4_dst' in stat.match:
+                        ip_dst = stat.match['ipv4_dst']
 
-                if packet_condition and byte_condition:
-                    # if ddos detected, send a flow rule to DROP packets
-                    self.add_flow(datapath=datapath, priority=11, match=stat.match, actions=[], idle_timeout=10)
-                    print(f'!!! DDOS detected !!!')
-                    # print(f'Dropping link to: {stat.match['eth_dst']}')
-                    print('Dropping link to: ', stat.match['eth_dst'])
-                
-                # send flow mod request to update the flow table
-                mod = parser.OFPFlowMod(datapath=datapath, priority=stat.priority, idle_timeout=stat.idle_timeout, match=stat.match, instructions=stat.instructions, flags=ofproto.OFPFF_RESET_COUNTS) # reset the counts for each flag
+                        if ip_dst not in self.dst_ip: 
+                            self.dst_ip[ip_dst] = 0
+                            print('added ip for: ', ip_dst)
+                        else:
+                            for dest_ip, dest_count in self.dst_ip.items():
+                                if ip_dst == dest_ip:
+                                    self.dst_ip[dest_ip] = dest_count + 1
+                                    print('incremented count for: ', ip_dst)
 
-                datapath.send_msg(mod)
+                    
+                    # check against firewall conditions
+                    packet_condition = stat.packet_count >= self.link_max
+                    byte_condition = (stat.byte_count / stat.packet_count) < self.byte_ratio_max
+
+                    if packet_condition and byte_condition:
+                        # if ddos detected, send a flow rule to DROP packets
+                        self.add_flow(datapath=datapath, priority=11, match=stat.match, actions=[], idle_timeout=10)
+                        print(f'!!! DDOS detected - packet or byte !!!')
+                        print(f'Dropping link to: ', stat.match['ipv4_dst'])
+                        visualize.display_attack_stats("H01", stat.match['ipv4_dst'], stat.packet_count, 1)
+
+                
+                    # send flow mod request to update the flow table
+                    mod = parser.OFPFlowMod(datapath=datapath, priority=stat.priority, idle_timeout=stat.idle_timeout, match=stat.match, instructions=stat.instructions, flags=ofproto.OFPFF_RESET_COUNTS) # reset the counts for each flag
+
+                    datapath.send_msg(mod)
         
         # check dst condition
-        if len(self.dst_mac) > 0:
-            for i in range(len(self.count_dst)):
-                dst_condition = self.count_dst[i] >= self.dst_max
+        if len(self.dst_ip) > 0:
+            for dest_ip, dest_count in self.dst_ip.items():
+                # print('ip: ', dest_ip)
+                # print('count: ', dest_count)
+                dst_condition = dest_count >= self.dst_max
                 if dst_condition:
-                    match = parser.OFPMatch(eth_dst=self.dst_mac[i])
+                    match = parser.OFPMatch(ipv4_dst=dest_ip)
                     # send DROP rule
-                    self.add_flow(datapath=datapath, priority=12, match=match, actions=[], idle_timeout=10)
+                    self.add_flow(datapath=datapath, priority=15, match=match, actions=[], idle_timeout=10)
+                    print(f'!!! DDOS detected - dst limit !!!')
+                    print(f'Dropping packets to dst mac: {dest_ip}')
+                    print('ip: ', dest_ip)
+                    print('count: ', dest_count)
 
-                    print(f'Dropping packets to dst mac: {self.dst_mac[i]}')
             
-            # reset all dst counts to 0
-            self.count_dst = [i * 0 for i in self.count_dst]
+        # reset all dst counts to 0
+        for key in self.dst_ip:
+            self.dst_ip[key] = 0
     
-    '''
-    add_dst
-        add an incoming dst mac address to the list of dst mac addresses
-    '''
-    def add_dst(self, src, dst):
-        if len(self.dst_mac) == 0:
-            self.dst_mac.append(dst) # add dest to list of destinations
-            self.count_dst.append(0) # initialise
-        else:
-            for i in range(len(self.dst_mac)):
-                if dst == self.dst_mac[i]:
-                    break # if dst already exists
-                if i == len(self.dst_mac) - 1:
-                    self.dst_mac.append(dst) # add new dst to end of list
-                    self.count_dst.append(0)
--- visualize.py
+++ visualize.py
@@ -1,10 +1,7 @@
 import requests
+import time
 
 def display_attack_stats(attacker_host, target_host, pkt_num, time_stamp):
-    # print(f"Update: {attacker_host}, {target_host}, {pkt_num}")
-
-    url = "http://127.0.0.1:5000/attack/table"
-    
     data = {
         "attacker_host": attacker_host,
         "target_host": target_host,
@@ -12,17 +9,34 @@ def display_attack_stats(attacker_host, target_host, pkt_num, time_stamp):
         "time_stamp": time_stamp
     }
     url = f"http://127.0.0.1:5000/attack/{attacker_host}"
-
     response = requests.put(url, json=data)
+    if response.status_code == 200:
+        print(f"Attack data updated for {attacker_host}")
+    else:
+        print(f"Failed to update attack data for {attacker_host}")
 
+def display_firewall_stats(attack_status, suspected_host, dropped_pkt_count, time_stamp):
+    data = {
+        "attack_status": attack_status,
+        "suspected_host": suspected_host,
+        "dropped_pkt_count": dropped_pkt_count,
+        "time_stamp": time_stamp
+    }
+    url = f"http://127.0.0.1:5000/firewall/{suspected_host}"
+    response = requests.put(url, json=data)
+    if response.status_code == 200:
+        print(f"Firewall data updated for {suspected_host}")
+    else:
+        print(f"Failed to update firewall data for {suspected_host}")
 
 if __name__ == '__main__':
-    import time
-
-    i=0
+    i = 0
     while i < 500:
         display_attack_stats("H01", "H11", i, time.time())
         display_attack_stats("H02", "H11", i+100, time.time())
+
+        attack_status = False
+        display_firewall_stats(attack_status, "H01", i, time.time())
+
         time.sleep(0.3)
-        i+=1
-    display_attack_stats("H03", "H11", i+100, time.time())
+        i += 1
