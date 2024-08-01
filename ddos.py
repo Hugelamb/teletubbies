143f4c7ef1067e571e3474b696cfa438ef9a4680
HugeLamb <hughrjlam@gmail.com>
2024-08-02 01:26:12 +1000

    functioning ddos attack on randomly selected host

---
+++ ddos.py
@@ -0,0 +1,78 @@
+
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.node import Controller, RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+# module imports
+import argparse
+import random 
+import threading
+import sys
+import csv
+import os
+import cmd
+from math import floor
+from subprocess import Popen
+from time import sleep,time
+# user import
+from topo_simple import SimpleTopo
+
+
+class AttackNet:
+    def __init__(self, net, intfs=[]):
+        self.attack_len = 2
+        self.tmp = 'tmp.txt'
+        self.intfs = intfs
+        self.data = {}
+        self.net = net
+
+    def clean_net(self):
+        cmd = "sudo mn -c"
+        Popen(cmd,shell=True).wait()
+
+    def init_attack(self):
+        """
+        Initiate DDoS attack with botnet_sz made of between 20-40% of network hosts using
+        hping3 in the background
+        """
+        info('*** Initiate attack\n')
+        net_sz = len(self.net.hosts)
+        botnet_sz = floor(net_sz*0.3) 
+        if botnet_sz < 2:
+            botnet_sz = 2
+        host_ids = [self.net.hosts[id] for id in random.sample(range(len(self.net.hosts)),botnet_sz)]
+        print(host_ids)
+        attackers = host_ids[1:]
+        target = host_ids[0]
+        print(target.IP())
+        print(f"The following hosts will be attacking during the simulation: {attackers}")
+        print(f"{target} will be the victim of the DDoS attack")
+        print(f"{target} has ip {target.IP()}")
+        for attacker in attackers:
+            
+            print(attacker)
+            attacker.cmd(f"hping3 --flood {target.IP()} &")
+            print("still running")
+
+    def end_attack(self):
+        info('*** End Attack\n')
+        cmd = "killall hping3"
+        Popen(cmd, shell=True).wait()
+    
+    def start_monitor(self):
+        info('*** run bwm-ng in background for data collection\n')
+        cmd = f"bwm-ng -o csv -T rate -C ',' > {self.tmp} &"
+        Popen(cmd,shell=True).wait()
+
+    def end_monitor(self):
+        """Kill all running instances of bwm-ng """
+        info('*** kill all monitoring instances\n')
+        cmd = f"killall bwm-ng"
+        Popen(cmd, shell=True).wait()
+
+    '''def run(self):
+        info('*** run ddos\n') 
+        self.clean_net()
+        self.init_attack()
+    ''' 
--- main.py
+++ main.py
@@ -4,21 +4,31 @@ from mininet.node import RemoteController
 from mininet.link import TCLink
 from mininet.cli import CLI
 from topo_simple import SimpleTopo
-from ddos_sim import ddos_attack
+import ddos
 import argparse
 import sys
-
+from mininet.log import setLogLevel,info
 def run(k):
-    
+
+    setLogLevel('info')
+
     topo = SimpleTopo(k)
     net = Mininet(topo, link=TCLink, controller=None, autoSetMacs=True, autoStaticArp=True)
-    net.addController('controller', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")
-    print('boo')
-    net.start()
-    print('boo')
-    CLI(net)
-    net.stop()
-
+    net.addController('controller', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")  
+    attackNet = ddos.AttackNet(net)
+    try:
+        net.start()
+        attackNet.start_monitor()
+        attackNet.init_attack()
+        
+        CLI(net)
+        attackNet.end_attack()
+        attackNet.end_monitor()
+        net.stop()
+        attackNet.clean_net()
+    except:
+        attackNet.clean_net()
+        print('error encountered')
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a simple topo with k hosts')
     parser.add_argument('k', type=int, nargs='?', default=4, help='Enter Hosts')
