89941b596bb1c6d5afc1b9c1e307badbcb2ba431
HugeLamb <hughrjlam@gmail.com>
2024-08-06 16:43:08 +1000

    added plots for k=4, as well as second argument to main.py to allow suppression of plots as they take some time to create

--- ddos.py
+++ ddos.py
@@ -10,6 +10,7 @@ import random
 import sys
 import csv
 import cmd
+import os
 import matplotlib.pyplot as plt
 from math import floor
 from subprocess import Popen
@@ -28,7 +29,9 @@ class AttackNet:
         self.itfs = []
         self.atk_start = 0
         self.atk_end = 0
-
+        self.actors = []
+        self.normals = []
+        self.target = ''
     def clean_net(self):
         cmd = "sudo mn -c"
         Popen(cmd,shell=True).wait()
@@ -39,25 +42,34 @@ class AttackNet:
 
         hping3 in the background
         """
-        info('*** Initiate attack\n')
+        info('*** Initiate simulation\n')
         net_sz = len(self.net.hosts)
         botnet_sz = floor(net_sz*0.3) 
         if botnet_sz < 2:
             botnet_sz = 2
         host_ids = [self.net.hosts[id] for id in random.sample(range(len(self.net.hosts)),botnet_sz)]
-        print(host_ids)
-        attackers = host_ids[1:]
-        target = host_ids[0]
-        print(target.IP())
-        print(f"The following hosts will be attacking during the simulation: {attackers}")
-        print(f"{target} will be the victim of the DDoS attack")
-        print(f"{target} has ip {target.IP()}")
-        for attacker in attackers:
-            
-            print(attacker)
-            attacker.cmd(f"hping3 --flood {target.IP()} &")
-            print("still running")
+        self.actors = host_ids
+        self.target = self.actors[0]
+        # print(self.actors)
+    def start_traffic(self):
+        info('*** Start normal network traffic\n')
+        self.normals = [id for id in self.net.hosts if not id in self.actors]
+        # run traffic for sim duration     
+        for host in self.normals:
+            host.cmd(f"timeout {self.wait_len*2+self.attack_len}s hping3 -S -d 200 -i 1 {self.target.IP()} &")      
+        
 
+    # def end_traffic(self):
+        
+
+    def start_attack(self):
+        info('*** Begin Attack\n')
+        attackers = self.actors[1:]
+        print(f"{self.target} will be the victim of the DDoS attack")
+        print(f"{self.target} has ip {self.target.IP()}")
+        for attacker in attackers:
+            attacker.cmd(f"timeout {self.attack_len}s hping3 --flood {self.target.IP()} &")
+    
     def end_attack(self):
         info('*** End Attack\n')
         cmd = "killall hping3"
@@ -73,33 +85,76 @@ class AttackNet:
         info('*** kill all monitoring instances\n')
         cmd = f"killall bwm-ng"
         Popen(cmd, shell=True).wait()
+
     def data_collection(self):
-        info('*** Scrape data from generated csv file')
+        info('*** Scrape data from generated csv file\n')
+        t0 = float(0)  # for removing offset for time (ie so start time is 0)
         with open(self.tmp) as data_csv:
-            reader = csv.reader(data_csv, delimiter=',')
+            reader = csv.reader(data_csv, delimiter=',', quoting=csv.QUOTE_NONE)
             for row in reader:
                 key = row[1]
                 timestamp = float(row[0])
+                rxb = float(row[2])
+                txb = float(row[3])
+                # txp = float(row[7])
+                # rxp = float(row[8])
                 thruput_bytes = float(row[4])
                 if key in self.data:
                     self.data[key]['time'].append(timestamp)
                     self.data[key]['load'].append(thruput_bytes)
+                    self.data[key]['Tx_bytes/s'].append(txb)
+                    self.data[key]['Rx_bytes/s'].append(rxb)
+                  # self.data[key]['Packets out/s'].append(txp)
+                  # self.data[key]['Packets in/s'].append(rxp)
+
+
                 else:
                     self.data[key] = {}
                     self.data[key]['time'] = []
                     self.data[key]['load'] = []
+                    self.data[key]['Tx_bytes/s'] = []
+                    self.data[key]['Rx_bytes/s'] = []
+                    self.data[key]['Packets out/s'] = []
+                    self.data[key]['Packets in/s'] = []
+            
     def data_plots(self):
         info('*** Plots\n')
-        itfs = [t for t in self.itfs if t in self.data]
+        intf_of_intrst = [[[x.intf1.name, x.intf2.name] for x in self.net.links if str(y) in str(x.intf1)] for y in self.actors]
+        intf_of_intrst = [x[0] for x in intf_of_intrst]
         attack_period = (self.atk_start,self.atk_end)
-        for k in sorted(self.data):
-            xdata = self.data[k]['time']
-            ydata = self.data[k]['load']
-            plt.plot(xdata,ydata)
-            plt.title(k)
-            plt.ylabel('bits/s')
-            plt.axvspan(*attack_period,color='red', alpha = 0.1)
-            plt.show()
+        counter = 0
+        intf_normal = [[[x.intf1.name, x.intf2.name] for x in self.net.links if str(y) in str(x.intf1)] for y in self.normals]
+        intf_of_intrst.append(intf_normal[random.randint(0,len(intf_normal)-1)][0]) 
+        for k in sorted(intf_of_intrst):
+            xdataraw = self.data[k[1]]['time']
+            xdata = [x-min(xdataraw) for x in xdataraw] 
+            ydata = self.data[k[1]]
+            atk_t_no_offset = (attack_period[0]-min(xdataraw),attack_period[1]-min(xdataraw))     
+            plt.figure()
+            loadline, = plt.plot(xdata,ydata['load'], label='Overall Load')
+            txb, = plt.plot(xdata,ydata['Tx_bytes/s'], label='Outgoing bytes')
+            rxb, = plt.plot(xdata,ydata['Rx_bytes/s'], label='Received bytes')
+            if k[0][0:-5] == str(self.target):
+                plt.title(f"Target {k[0][0:-5]} network activity")
+                
+            elif k[0][0:-5] in str(self.actors): 
+                plt.title(f"Attacker {k[0][0:-5]} network activity")
+            else:
+                plt.title(f"Normal host {k[0][0:-5]} network activity")
+            print(k)
+            plt.ylabel('Port Activity (bytes/s)')
+            plt.xlabel('Simulation Time (s)')
+            plt.legend(handles=[loadline,txb,rxb])
+            plt.axvspan(*atk_t_no_offset,color='red', alpha = 0.1)
+            cwd = os.getcwd()
+            imagepath = cwd + "/plots/"+ f'{k[0][0:-5]}.png' 
+            plt.savefig(imagepath, bbox_inches='tight')
+            if counter < len(intf_of_intrst)-1:
+                plt.show(block=False)
+            else:
+                plt.show()
+            counter += 1
+            
                 
     '''def run(self):
         info('*** run ddos\n') 
--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.link import TCLink
 from mininet.cli import CLI
+from mininet.log import setLogLevel,info
 from topo_simple import SimpleTopo
 from ddos import AttackNet
 import subprocess
 import argparse
 import sys
+import traceback
 from time import sleep,time
 #   import website
 
-def run(k):
-    
-    
+def run(k,plot='n'):
     # subprocess.Popen(['ryu-manager', 'ryu_firewall.py', '--log-dir', 'logs', '--log-file', 'ryu.log'])
     # subprocess.Popen(['ryu-manager', 'visualize.py', '--log-dir', 'logs', '--log-file', 'ryu.log'])
 
@@ -42,28 +42,36 @@ def run(k):
     net.addController('controller', controller=RemoteController, ip="127.0.0.1", port=6633, protocols="OpenFlow13")
     net.start()
     try:
+        setLogLevel('info')
         atk = AttackNet(net)
-        atk.start_monitor() 
+        atk.start_monitor()
         sleep(atk.wait_len)
-        atk.init_attack()
+        atk.init_attack()       # initialize host roles
+        atk.start_traffic()
+        atk.start_attack()      # begin attacking
         atk.atk_start = time()
-        sleep(atk.attack_len)
-        atk.end_attack()
+        sleep(atk.attack_len)   # wait for given attack duration
+        # atk.end_attack()
         atk.atk_end = time()
         sleep(atk.wait_len)
+        # atk.end_traffic()       # kill all regular network traffic
         atk.end_monitor()
         atk.data_collection()
-        atk.data_plots()
+        if plot == 'y':
+            atk.data_plots()
         CLI(net)
         net.stop()
         atk.clean_net()
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt,UserWarning,Exception) as e:
+        print(traceback.format_exc())
+        print(f"{e} Error Occurred while running attack simulation\n")
         net.stop()
         atk.clean_net()
-        print("Error Occurred while running attack simulation")
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a simple topo with k hosts')
     parser.add_argument('k', type=int, nargs='?', default=4, help='Enter Hosts')
+    parser.add_argument('plot',type=str, nargs='?', default='n', help='Should plots be generated? [y/n]')
     args = parser.parse_args()
 
-    run(args.k)
+    run(args.k,args.plot)
